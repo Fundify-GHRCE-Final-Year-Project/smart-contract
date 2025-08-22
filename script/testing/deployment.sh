32d13006e3363b0b2702b8fd3fe7d416a29592d7
bhivgadearav <hazeifygameplay5927@gmail.com>
2025-08-22 17:07:47 +0530

    updated testing script to make more projects and investments

--- script/testing/deployment.sh
+++ script/testing/deployment.sh
@@ -4,13 +4,19 @@ set -e
 # Start local Anvil if not running by running command "anvil"
 # If you installed foundry it will be available by default in your terminal
 
-# Export your deployer private key (Anvil's default first account)
-export PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-export FUNDIFY_ADDRESS=0x9E60F7933Ad5Fcc9A77efb05E501ba8909F64299
+# Load environment variables from .env if it exists
+if [ -f ".env.development" ]; then
+  echo "Sourcing .env.development..."
+  set -a
+  source .env.development
+  set +a
+fi
+
+USER1_PRIVATE_KEY='0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 
 # Deploy and create projects in one go
-forge script script/testing/testing.s.sol:DeployAndCreateFundify \
+forge script testing.s.sol:DeployAndCreateFundify \
     --rpc-url http://localhost:8545 \
     --broadcast \
-    --private-key $PRIVATE_KEY
+    --private-key $USER1_PRIVATE_KEY
 
--- script/testing/testing.s.sol
+++ script/testing/testing.s.sol
@@ -6,20 +6,149 @@ import "../../src/Fundify.sol";
 
 contract DeployAndCreateFundify is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 user1PrivateKey = vm.envUint("USER1_PRIVATE_KEY");
+        address user1PublicKey = vm.addr(user1PrivateKey);
 
-        vm.startBroadcast(deployerPrivateKey);
+        uint256 user2PrivateKey = vm.envUint("USER2_PRIVATE_KEY");
+        address user2PublicKey = vm.addr(user2PrivateKey);
 
-        // Deploy Fundify
+        uint256 user3PrivateKey = vm.envUint("USER3_PRIVATE_KEY");
+        address user3PublicKey = vm.addr(user3PrivateKey);
+
+        uint256 user4PrivateKey = vm.envUint("USER4_PRIVATE_KEY");
+        address user4PublicKey = vm.addr(user4PrivateKey);
+
+        uint256 user5PrivateKey = vm.envUint("USER5_PRIVATE_KEY");
+        address user5PublicKey = vm.addr(user5PrivateKey);
+
+        vm.startBroadcast(user1PrivateKey);
+
         Fundify fundify = new Fundify();
         fundify.initialize();
         console.log("Fundify deployed at:", address(fundify));
 
-        // Create 3 projects
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user1PrivateKey);
+
         fundify.createProject(10 ether, 2);
         fundify.createProject(5 ether, 3);
         fundify.createProject(20 ether, 4);
         console.log("Created 3 projects");
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user2PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user1PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user3PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user1PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user4PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user1PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user5PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user1PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user1PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user2PrivateKey);
+
+        fundify.createProject(10 ether, 2);
+        fundify.createProject(5 ether, 3);
+        fundify.createProject(20 ether, 4);
+        console.log("Created 3 projects");
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user2PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user2PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user3PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user2PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user4PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user2PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user5PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user2PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user2PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user3PrivateKey);
+
+        fundify.createProject(10 ether, 2);
+        fundify.createProject(5 ether, 3);
+        fundify.createProject(20 ether, 4);
+        console.log("Created 3 projects");
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user2PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user3PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user3PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user3PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user4PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user3PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 2);
+
+        vm.stopBroadcast();
+
+        vm.startBroadcast(user5PrivateKey);
+
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 0);
+        fundify.fundProject{value: 1 ether}(user3PublicKey, 1);
+        fundify.fundProject{value: 2 ether}(user3PublicKey, 2);
 
         vm.stopBroadcast();
     }
